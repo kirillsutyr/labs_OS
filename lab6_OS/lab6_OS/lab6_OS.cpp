ba48b1af4ac27c1cc8594761160445d50af60142
Sutyr Kyrylo <kirillsutyr@gmail.com>
2020-05-24 15:44:12 +0300

    lab 6

---
+++ lab6_OS/lab6_OS/lab6_OS.cpp
@@ -0,0 +1,33 @@
+﻿#include<stdio.h>
+#include <windows.h>
+
+#include <string>
+
+void DrawCat()
+{
+	for (int i = 0; i < 1000; i++)
+	{
+		//process of drawing
+		Sleep(2);
+	}
+}
+
+void DrawDog()
+{
+	for (int i = 0; i < 1000; i++)
+	{
+		//process of drawing
+		Sleep(2);
+	}
+}
+           
+int main()
+{
+	printf("\n Inside main()\n");
+	int typeOfShape = 0;
+	scanf_s("%i", &typeOfShape);
+
+	typeOfShape == 1 ? DrawDog : DrawCat;
+
+	return 0;
+}
